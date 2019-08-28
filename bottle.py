3864c76d795cc7943136f0b42e8f6c70b1a5e576
adesh12 <adeshchauhan22@gmail.com>
2019-08-28 17:41:12 +0530

    initial commit

---
+++ bottle.py
@@ -0,0 +1,14 @@
+def min_visible_bottles(arr, n):
+
+    m = dict()
+    ans=0
+    for i in range(n):
+        m[arr[i]] = m.get(arr[i], 0) + 1
+        ans = max(ans, m[arr[i]])
+
+    print(ans)
+
+n = 8
+arr = [1, 1, 2, 3, 4, 5, 5, 4]
+
+min_visible_bottles(arr, n)
